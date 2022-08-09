61979abc7aa58feeff941cd4100391367630020f
MatValdejao <matvaldejao123@gmail.com>
2022-08-09 12:11:05 -0400

    added tests for methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,22 @@ test("creates an employee object", () => {
     expect(employee.email).toBe("dave@email.com");
     expect(employee.id).toBe(123);
 });
+
+test("gets employee name", () => {
+    const employee = new Employee("Dave", "dave@email.com", 123);
+
+    expect(employee.getName()).toBe("Dave")
+})
+
+
+test("gets employee email", () => {
+    const employee = new Employee("Dave", "dave@email.com", 123);
+
+    expect(employee.getEmail()).toBe("dave@email.com")
+})
+
+test("gets employee id number", () => {
+    const employee = new Employee("Dave", "dave@email.com", 123);
+
+    expect(employee.getId()).toBe(123)
+})
