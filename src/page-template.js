9dd681c47f0d7df2e267e1a5df38c9133b698e35
MatValdejao <matvaldejao123@gmail.com>
2022-08-09 19:03:34 -0400

    setup basic template

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,35 @@
-const generateHTML = (employeesArr) => {
+const generateEmployeeItems = (employeesArr) => {
     
 }
+
+const generateHTML = (employeesArr) => {
+    `<!DOCTYPE html> 
+    <html lang="en"> 
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Members</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="header">
+                <h1 class="page-title">
+                    My Team
+                </h1>
+            </div>
+        </header>
+        <main class="container">
+            <div class="row">
+                ${console.log("will run generation of card items in page")}
+            </div>                       
+        </main>
+    </body>
+    </html>
+    `;
+};
