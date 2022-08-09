c94ea34179a20e5599e73a73bb8405c680f38348
MatValdejao <matvaldejao123@gmail.com>
2022-08-09 16:23:47 -0400

    engineer tests built

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,5 +2,19 @@ const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
 test("constructs engineer object", () => {
-    
+    const engineer = new Engineer("davegit")
+
+    expect(engineer.github).toBe("davegit")
+})
+
+test("gets github username", () => {
+    const engineer = new Engineer("davegit")
+
+    expect(engineer.getGithub()).toBe("davegit")
+})
+
+test("gets employee company role", () => {
+    const engineer = new Engineer("davegit")
+
+    expect(engineer.getRole()).toBe("Engineer")
 })
