7aeaa0a98cef20c6b1d6f3524a3acbfca832b82b
MatValdejao <matvaldejao123@gmail.com>
2022-08-09 16:27:30 -0400

    changed id param to be string to facilitate with inquirer usage

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,26 +9,26 @@ test("creates an employee object", () => {
 });
 
 test("gets employee name", () => {
-    const employee = new Employee("Dave", "dave@email.com", 123);
+    const employee = new Employee("Dave", "dave@email.com", "123");
 
     expect(employee.getName()).toBe("Dave")
 })
 
 
 test("gets employee email", () => {
-    const employee = new Employee("Dave", "dave@email.com", 123);
+    const employee = new Employee("Dave", "dave@email.com", "123");
 
     expect(employee.getEmail()).toBe("dave@email.com")
 })
 
 test("gets employee id number", () => {
-    const employee = new Employee("Dave", "dave@email.com", 123);
+    const employee = new Employee("Dave", "dave@email.com", "123");
 
-    expect(employee.getId()).toBe(123)
+    expect(employee.getId()).toBe("123")
 })
 
 test("gets employee role", () => {
-    const employee = new Employee("Dave", "dave@email.com", 123)
+    const employee = new Employee("Dave", "dave@email.com", "123")
 
     expect(employee.getRole()).toBe("Employee")
 
